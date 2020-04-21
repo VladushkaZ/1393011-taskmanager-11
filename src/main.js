a4d96c705c4d9612906c15f464607c7d80cb6f6b
Vlada Zorina <vlada2227@mail.ru>
2020-04-22 00:01:18 +0300

    Генерация разметки

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import {createSiteMenuTemplate} from "./components/menu.js";
-import {createFilterTemplate} from "./components/filter.js";
 import {createTasksTemplate} from "./components/tasks.js";
 import {createEditTaskTemplate} from "./components/edit-task.js";
 import {createButtonTemplate} from "./components/button.js";
 import {createBoardTemplate} from "./components/board.js";
-
+import {createFilterTemplate} from "./components/filter.js";
+import {generateFilters} from "./mock/filter.js";
+import {generateTasks} from "./mock/tasks";
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -13,17 +14,35 @@ const siteMainElement = document.querySelector(`.main`);
 
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 render(siteHeaderElement, createSiteMenuTemplate());
-
-render(siteMainElement, createFilterTemplate());
+const filters = generateFilters();
+render(siteMainElement, createFilterTemplate(filters));
+const TASK_OUT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 render(siteMainElement, createBoardTemplate());
-const TASK_OUT = 3;
+const tasks = generateTasks(TASK_OUT);
+
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const boardElement = siteMainElement.querySelector(`.board`);
 
-render(taskListElement, createEditTaskTemplate());
-for (let i = 0; i < TASK_OUT; i++) {
-  render(taskListElement, createTasksTemplate());
-}
+render(taskListElement, createEditTaskTemplate(tasks[0]));
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+tasks.slice(1, showingTasksCount)
+  .forEach((task) => render(taskListElement, createTasksTemplate(task), `beforeend`));
 
 render(boardElement, createButtonTemplate());
+
+const loadButton = boardElement.querySelector(`.load-more`);
+
+loadButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(taskListElement, createTasksTemplate(task), `beforeend`));
+
+  if (showingTasksCount >= tasks.length) {
+    loadButton.remove();
+  }
+});
